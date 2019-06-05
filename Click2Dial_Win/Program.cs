e8e940fb50bb64168d67b5317bb52a57fbe716b6
absolte <Hobgoodc1@gmail.com>
2019-06-04 22:51:08 -0400

    Base Code for C2D_Win

---
+++ Click2Dial_Win/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Click2Dial_Win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path + @"\test.txt"))
+                {
+                    // Pass the phone number through cmd args to C# for text file.
+                    foreach (var arg in args)
+                    {
+                        sw.WriteLine(arg);
+                    }
+                }
+            }
+        
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path + @"\test.txt"))
+            {
+                string s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
+
